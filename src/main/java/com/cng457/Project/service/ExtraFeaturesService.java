5f4ac472b57cc18961cba4ec7e2874c8447cbe1a
Kerem <kerem.comert@appcent.mobi>
2021-05-31 21:33:26 +0300

    Create ExtraFeatures repository, service, controller

---
+++ src/main/java/com/cng457/Project/service/ExtraFeaturesService.java
@@ -0,0 +1,20 @@
+package com.cng457.Project.service;
+
+import com.cng457.Project.entity.ExtraFeatures;
+import com.cng457.Project.repository.ExtraFeaturesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExtraFeaturesService {
+    @Autowired
+    ExtraFeaturesRepository extraFeaturesRepository;
+
+    public ExtraFeatures saveExtraFeatures(ExtraFeatures ef){
+        return extraFeaturesRepository.save(ef);
+    }
+
+    public ExtraFeatures getExtraFeatures(int efId){
+        return extraFeaturesRepository.findById(efId).orElse(null);
+    }
+}
