45f0fa53148802a723e2be477ec69f7e7514e4f9
forumber <kidobeko@gmail.com>
2021-06-20 17:33:15 +0300

    getAllBrands()

--- src/main/java/com/cng457/Project/controller/BrandController.java
+++ src/main/java/com/cng457/Project/controller/BrandController.java
@@ -1,9 +1,12 @@
 package com.cng457.Project.controller;
 
 import com.cng457.Project.entity.Brand;
+import com.cng457.Project.entity.Computer;
 import com.cng457.Project.service.BrandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class BrandController {
@@ -18,5 +21,10 @@ public class BrandController {
     @GetMapping("/getBrand/{id}")
     public Brand getBrand(@PathVariable int id) {
         return brandService.getBrand(id);
+    }
+
+    @GetMapping("/getAllBrands")
+    public List<Brand> getAllBrands() {
+        return brandService.getAllBrands();
     }
 }
--- src/main/java/com/cng457/Project/repository/BrandRepository.java
+++ src/main/java/com/cng457/Project/repository/BrandRepository.java
@@ -1,8 +1,13 @@
 package com.cng457.Project.repository;
 
 import com.cng457.Project.entity.Brand;
+import com.cng457.Project.entity.Computer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
-
+    @Query(value = "SELECT * FROM brand p", nativeQuery = true)
+    public List<Brand> getAllBrands();
 }
