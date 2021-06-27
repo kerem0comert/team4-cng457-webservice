8d94f703a42a8b3858fde0d2beaed88e91be0fb3
forumber <kidobeko@gmail.com>
2021-06-27 12:29:48 +0300

    Introduce code-sided locking

--- src/main/java/com/cng457/Project/service/BrandService.java
+++ src/main/java/com/cng457/Project/service/BrandService.java
@@ -1,27 +1,44 @@
 package com.cng457.Project.service;
 
 import com.cng457.Project.entity.Brand;
-import com.cng457.Project.entity.Computer;
 import com.cng457.Project.repository.BrandRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 @Service
 public class BrandService {
     @Autowired
     BrandRepository brandRepository;
 
+    ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
+
     public Brand saveBrand(Brand b) {
-        return brandRepository.save(b);
+        lock.writeLock().lock();
+        try {
+            return brandRepository.save(b);
+        } finally {
+            lock.writeLock().unlock();
+        }
     }
 
     public Brand getBrand(int id) {
-        return brandRepository.findById(id).orElse(null);
+        lock.readLock().lock();
+        try {
+            return brandRepository.findById(id).orElse(null);
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 
     public List<Brand> getAllBrands() {
-        return brandRepository.getAllBrands();
+        lock.readLock().lock();
+        try {
+            return brandRepository.getAllBrands();
+        } finally {
+            lock.readLock().unlock();
+        }
     }
 }
